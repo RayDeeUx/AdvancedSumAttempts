7ed712b4332665c07469b493da28d24c893c4198
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2026-01-07 23:23:44 -0500

    disclaimer

--- src/main.cpp
+++ src/main.cpp
@@ -114,7 +114,7 @@ void findSumAndDisplay(CCArray* array, const ArrayType type, GJSearchObject* gjs
 	}
 
 	const std::string& warning = levels != array->count() ? fmt::format("\n\n<co>This information is incomplete. Try downloading {} more level{}, then check back later!</c>", (array->count() - levels), (array->count() - levels > 1) ? "s" : "") : "";
-	const std::string& timestampsString = (timestamps == minTimestamps && timestamps == maxTimestamps && minTimestamps == maxTimestamps) ? fmt::format("It will take at least <cy>{} second{}</c> to beat the <cb>{} level{}</c> available.", timestamps / 240, timestamps / 240 != 1 ? "s" : "", levels, levels != 1 ? "s" : "") : fmt::format("It will take somewhere between <cg>{} second{}</c> and <cr>{} second{}</c> (calculated <cy>{} second{}</c>) to beat the <cb>{} level{}</c> available.", minTimestamps / 240, minTimestamps / 240 != 1 ? "s" : "", maxTimestamps / 240, maxTimestamps / 240 != 1 ? "s" : "", timestamps / 240, timestamps / 240 != 1 ? "s" : "", levels, levels != 1 ? "s" : "");
+	const std::string& timestampsString = (timestamps == minTimestamps && timestamps == maxTimestamps && minTimestamps == maxTimestamps) ? fmt::format("It will take at least <cy>{} second{}</c> to beat the <cb>{} level{}</c> available.", timestamps / 240, timestamps / 240 != 1 ? "s" : "", levels, levels != 1 ? "s" : "") : fmt::format("It will take somewhere between <cg>{} second{}</c> and <cr>{} second{}</c> (calculated <cy>{} second{}</c>) to beat the <cb>{} level{}</c> available.{}", minTimestamps / 240, minTimestamps / 240 != 1 ? "s" : "", maxTimestamps / 240, maxTimestamps / 240 != 1 ? "s" : "", timestamps / 240, timestamps / 240 != 1 ? "s" : "", levels, levels != 1 ? "s" : "", timestamps > 0 ? "" : "\n\n(This estimate is <c_>VERY</c> rough, as none of these levels have a known level duration Try viewing them individually using the BetterInfo mod.)");
 
 	FLAlertLayer* alert = FLAlertLayer::create(
 		nullptr,
@@ -123,7 +123,7 @@ void findSumAndDisplay(CCArray* array, const ArrayType type, GJSearchObject* gjs
 			"You have <cc>{} attempt{}</c>, <cl>{} jump{}</c>, "
 			"<cd>{} click{}</c>, <cs>{} collected star{}</c>, "
 			"and <cj>{} collected moon{}</c> "
-			"across <cb>{} available level{}</c> (of which <cg>{} completed</c>, out of <cf>{} total</c>).\n\n"
+			"across <cb>{} available level{}</c> (of which <cg>{} are completed</c>, out of <cf>{} total</c>).\n\n"
 			"These levels use a total of at least <ca>{} object{}</c> "
 			"and are worth <cs>{} star{}</c> and <cj>{} moon{}</c> in total.\n\n"
 			"{}{}",
